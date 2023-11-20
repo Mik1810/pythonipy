eee9bc53e29a9c18e5de57ee2009c8cd84b5e06f
Mik1810 <michael.piccirilli@student.univaq.it>
2023-11-20 22:06:37 +0100

    Updated the project, now ha a more intercative way to start and is more intuitive

--- keyboard_interrupt.py
+++ keyboard_interrupt.py
@@ -1,34 +1,36 @@
 import keyboard
 from spotipy import SpotifyException
-sp1 = None
-paused = False
 
 
-def initializeSpotify(sp=None):
-    global sp1
-    sp1 = sp
+class HotkeyListener():
+    pause: bool = False
+    sp = None
 
+    def __init__(self, sp):
+        if sp is None:
+            raise RuntimeError("You have to pass a Spotify Client!")
+        self.sp = sp
 
-def on_key_press(event):
-    global sp1, paused
-    try:
-        if keyboard.is_pressed('ctrl') and keyboard.is_pressed('alt') and keyboard.is_pressed('right'):
-            print("CTRL + Alt + Freccia destra premuti contemporaneamente.")
-            sp1.next_track()
-        if keyboard.is_pressed('ctrl') and keyboard.is_pressed('alt') and keyboard.is_pressed('space'):
-            if paused:
-                sp1.start_playback()
-            else:
-                sp1.pause_playback()
-            paused = not paused
-        if keyboard.is_pressed('ctrl') and keyboard.is_pressed('alt') and keyboard.is_pressed('p'):
-            print("Added to favorites")
-            file = open('songs.txt', 'a+')
-            file.write("LIKED ")
-    except SpotifyException as e:
-        print("You cannot have PREMIUM!")
+    def on_key_press(self, event):
+        try:
+            """
+            if keyboard.is_pressed('ctrl') and keyboard.is_pressed('alt') and keyboard.is_pressed('right'):
+                print("CTRL + Alt + Freccia destra premuti contemporaneamente.")
+                self.sp.next_track()
+            if keyboard.is_pressed('ctrl') and keyboard.is_pressed('alt') and keyboard.is_pressed('space'):
+                if self.pause:
+                    self.sp.start_playback()
+                else:
+                    self.sp.pause_playback()
+                self.pause = not self.pause
+            """
+            if keyboard.is_pressed('ctrl') and keyboard.is_pressed('alt') and keyboard.is_pressed('p'):
+                print("Added to favorites")
+                with open('songs.txt', 'rb+') as file:
+                    file.seek(-2, 2)
+                    file.write(' LIKED\n'.encode())
+        except SpotifyException:
+            print("You don't have PREMIUM!")
 
-
-
-
-keyboard.on_press(on_key_press)
+    def listen(self):
+        keyboard.on_press(self.on_key_press)
--- main.py
+++ main.py
@@ -2,57 +2,73 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import time
 from datetime import datetime
-from keyboard_interrupt import initializeSpotify
+from keyboard_interrupt import HotkeyListener
 import json
 
-credentials_path = "./credentials.json"
+credentials_path = "credentials.json"
+scope = "user-read-currently-playing user-modify-playback-state"
+
+
 def get_credentials():
     with open(credentials_path, "r") as keys:
         data = json.load(keys)
     return data['client-id'], data['secret-id'], data['redirect-url']
 
-scope = "user-read-currently-playing user-modify-playback-state"
 
-client, secret, redirect = get_credentials()
+def store_song(sp):
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    client_id=client,
-    client_secret=secret,
-    redirect_uri=redirect,
-    scope=scope))
+    previous_track_id = None
+    try:
+        while True:
+            current_track = sp.current_user_playing_track()
 
-previous_track_id = None
-file = None
+            if current_track is not None:
+                current_track_id = current_track['item']['id']
+                if current_track_id != previous_track_id:
+                    # Mi interessa salvare il nome dell'artista item -> artists -> 0 -> name
+                    # il nome della canzone item -> name
+                    data_ora_corrente = datetime.now()
+                    # Formatta la data e l'orario corrente tra parentesi quadre
+                    data_ora_formattata = "[" + data_ora_corrente.strftime("%Y-%m-%d %H:%M:%S") + "]"
+                    with open('songs.txt', 'a') as file:
+                        file.write(data_ora_formattata + " " +
+                                   current_track['item']['name'] + " - " + current_track['item']['artists'][0]['name'])
+                        file.write("\n")
+                    print(data_ora_formattata, " ",
+                          current_track['item']['name'] + " - " + current_track['item']['artists'][0]['name'])
 
-# Initialize spotify for hotkeys listener
-initializeSpotify(sp)
+                previous_track_id = current_track_id
 
-try:
+            time.sleep(5)  # Polling interval
+    except KeyboardInterrupt:
+        print("Esecuzione interrotta")
+        with open('songs.txt', 'a') as file:
+            file.write("\n")
+    finally:
+        file.close()
 
-    file = open('songs.txt', 'a', encoding='utf-8')
-    while True:
 
-        current_track = sp.current_user_playing_track()
+if __name__ == "__main__":
+    selected = int(input("1. Use credentials.json\n2. Pass it manually\n"))
+    if selected == 1:
+        client, secret, redirect = get_credentials()
 
-        if current_track is not None:
-            current_track_id = current_track['item']['id']
-            if current_track_id != previous_track_id:
-                # Mi interessa salvare il nome dell'artista item -> artists -> 0 -> name
-                # il nome della canzone item -> name
-                data_ora_corrente = datetime.now()
-                # Formatta la data e l'orario corrente tra parentesi quadre
-                data_ora_formattata = "[" + data_ora_corrente.strftime("%Y-%m-%d %H:%M:%S") + "]"
-                file.write(data_ora_formattata+ " "+
-                           current_track['item']['name'] + " - " + current_track['item']['artists'][0]['name'])
-                file.write("\n")
-                print(data_ora_formattata, " ",
-                        current_track['item']['name'] + " - " + current_track['item']['artists'][0]['name'])
+        # Initialize spotify client
+        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+            client_id=client,
+            client_secret=secret,
+            redirect_uri=redirect,
+            scope=scope))
 
-            previous_track_id = current_track_id
+        # Initialize spotify for hotkeys listener
+        listener = HotkeyListener(sp)
+        listener.listen()
 
-        time.sleep(5)  # Intervallo di polling (5 secondi nel caso di questo esempio)
-except KeyboardInterrupt:
-    print("Esecuzione interrotta")
-    file.write("\n")
-finally:
-    file.close()
+        # Store songs data
+        store_song(sp)
+
+    elif selected == 2:
+        # TODO: implement manually storing credentials
+        pass
+    else:
+        raise ValueError("Choose 1 or 2!")
